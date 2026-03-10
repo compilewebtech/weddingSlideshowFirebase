6c020faca530d201ef7efc2d9f53af79a5bdf193
saviokh <savio.khoury3@gmail.com>
2026-03-10 20:51:07 +0200

    fixes

--- client/src/firebase-config.ts
+++ client/src/firebase-config.ts
@@ -1,8 +1,8 @@
 export const firebaseConfig = {
-  apiKey: import.meta.env.VITE_FIREBASE_API_KEY ?? 'REPLACE_WITH_YOUR_API_KEY',
-  authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN ?? 'REPLACE_WITH_YOUR_PROJECT.firebaseapp.com',
-  projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID ?? 'REPLACE_WITH_YOUR_PROJECT_ID',
-  storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET ?? 'REPLACE_WITH_YOUR_PROJECT.appspot.com',
-  messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID ?? 'REPLACE_WITH_YOUR_SENDER_ID',
-  appId: import.meta.env.VITE_FIREBASE_APP_ID ?? 'REPLACE_WITH_YOUR_APP_ID',
+  apiKey: "AIzaSyCVuSJhF0oIWcmqK28KYSt5tD5NabqiwXA",
+  authDomain: "wedding-invitation-slideshow.firebaseapp.com",
+  projectId: "wedding-invitation-slideshow",
+  storageBucket: "wedding-invitation-slideshow.firebasestorage.app",
+  messagingSenderId: "894612163571",
+  appId: "1:894612163571:web:3dd6329838a3946c98e086"
 };
